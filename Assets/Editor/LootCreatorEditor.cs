18d3992710460cc1c83f77c3400500a3dd0d68ea
Chris Mills <war.ruins@gmail.com>
2021-03-25 15:40:25 -0700

    Refactor lootable creator

--- Assets/Editor/LootCreatorEditor.cs
+++ Assets/Editor/LootCreatorEditor.cs
@@ -15,14 +15,11 @@ public class LootCreatorEditor : EditorWindow
     // public enum GameContentOptions selection;
     private string assetsPath = "Assets/Resources/Loot/{0}.asset";
     private string itemType = "Loot";
-    private string itemName;
-    private string itemDescription;
-    private Sprite itemIcon;
+    private Lootable lootable;
     
     public float labelWidth = 150f;
-    public Lootable lootable;
     
-    [MenuItem(("Game/Loot Creator"))]
+    [MenuItem("Game/Loot Creator")]
     public static void ShowWindow()
     {
         EditorWindow window = GetWindow(typeof(LootCreatorEditor));
@@ -31,45 +28,46 @@ public class LootCreatorEditor : EditorWindow
         window.titleContent = titleContent;
     }
 
+    private void OnEnable()
+    {
+        CreateLootableInstance();
+    }
+
     private void OnGUI()
     {
-        DisplayQuestOptions();
+        DisplayLootableOptions();
 
         GUILayout.Space(20f);
         if (GUILayout.Button("Save"))
         {
-            CreateNewLoot();
+            SaveLootable();
             ClearForm();
         }
     }
 
-    void CreateNewLoot()
+    void CreateLootableInstance()
     {
-        // Create a new Quest ScriptableObject
-        Lootable loot = CreateInstance<Lootable>();
-        
-        // Update its properties with the form information
-        loot.itemType = itemType;
-        loot.itemName = itemName;  // TODO: Clean this double naming thing up!
-        loot.itemDescription = itemDescription;
-        loot.icon = itemIcon;
-        // Now that the info is entered on the quest, generate an ID with it.
-        loot.GenerateId();                 // TODO: Automate this.
+        lootable = CreateInstance<Lootable>();
+        lootable.itemType = itemType;
+    }
+
+    void SaveLootable()
+    {
+        lootable.GenerateId();                 // TODO: Automate this.
         
         // Finally, save the new asset to the specified location.
-        var path = String.Format(assetsPath, loot.itemId);
+        var path = String.Format(assetsPath, lootable.itemId);
         var newTitle = AssetDatabase.GenerateUniqueAssetPath(path);
-        AssetDatabase.CreateAsset(loot, newTitle);
+        AssetDatabase.CreateAsset(lootable, newTitle);
         AssetDatabase.SaveAssets();
     }
 
     void ClearForm()
     {
-        itemName = EditorGUILayout.TextField("");
-        itemDescription = EditorGUILayout.TextField("");
+        CreateLootableInstance();
     }
 
-    void DisplayQuestOptions()
+    void DisplayLootableOptions()
     {
         GUILayout.BeginHorizontal();
         GUILayout.Label("Loot Options", EditorStyles.boldLabel);
@@ -78,21 +76,21 @@ public class LootCreatorEditor : EditorWindow
         
         GUILayout.BeginHorizontal();
         GUILayout.Label("Item Name", GUILayout.Width(labelWidth));
-        itemName = EditorGUILayout.TextField(itemName);
+        lootable.itemName = EditorGUILayout.TextField(lootable.itemName);
         GUILayout.EndHorizontal();
         
         GUILayout.Space(5);
         
         GUILayout.BeginHorizontal();
         GUILayout.Label("Description", GUILayout.Width(labelWidth));
-        itemDescription = EditorGUILayout.TextArea(itemDescription,GUILayout.Height(100));
+        lootable.itemDescription = EditorGUILayout.TextArea(lootable.itemDescription,GUILayout.Height(100));
         GUILayout.EndHorizontal();
         
         GUILayout.Space(5);
         
         GUILayout.BeginHorizontal();
         GUILayout.Label("Icon", GUILayout.Width(labelWidth));
-        itemIcon = EditorGUILayout.ObjectField(itemIcon, typeof(Sprite), true) as Sprite;
+        lootable.icon = EditorGUILayout.ObjectField(lootable.icon, typeof(Sprite), true) as Sprite;
         GUILayout.EndHorizontal();
         
     }
